1d1905d4143be2315961c2c6a1f0acfe79c3a54a
MinPinSunHwa <37681149+MinPinSunHwa@users.noreply.github.com>
2018-06-26 21:15:31 +0900

    update

--- 03_Training.py
+++ 03_Training.py
@@ -188,3 +188,156 @@ print(tree_importance2)
 
 forest_importance1 = forest(train, tree_importance1, 5, 5, 100)
 
+
+
+
+
+
+
+
+
+def tree_(train, features, max_depth):
+    tree_importance = []
+    i = 0
+    columns = ['max_depth', 'train_RMSLE', 'test_RMSLE', 'important_features']
+    result = pd.DataFrame(columns=columns)
+    
+    
+    # Divid Train Data
+    X_train, X_test, y_train, y_test = train_test_split(train[features], np.log(train['target']), random_state=0)
+
+
+    # Train the Model Using Decision Tree
+    print('Train the Model Using Decision Tree')
+    
+    for d in range(1, max_depth+1):
+        print('\nmax_depth = {}'.format(d))
+        
+        tree = DecisionTreeRegressor(max_depth=d, random_state=0)
+        tree.fit(X_train, y_train)
+        
+        train_pred = tree.predict(X_train)
+        test_pred = tree.predict(X_test)
+        
+        
+        # Calculate RMSLE
+        train_rmsle = RMSLE(np.exp(y_train), np.exp(train_pred))
+        test_rmsle = RMSLE(np.exp(y_test), np.exp(test_pred))
+        
+        print('RMSLE of Train Set : ', train_rmsle)
+        print('RMSLE of Test Set : {}\n'.format(test_rmsle))
+        
+        
+        # Check for Important Features
+        feat = check_important_features(tree.feature_importances_, train.columns)
+        tree_importance.append(feat)
+
+        
+        # Save Result
+        r = pd.DataFrame({'max_depth':d, 'train_RMSLE':train_rmsle, 'test_RMSLE':test_rmsle,
+                          'important_features':', '.join(feat)}, 
+                         columns=columns, index=[i])
+        result = pd.concat([result, r], ignore_index=True)
+        i += 1
+        
+    return tree_importance, result
+    
+
+def forest_(train, features, max_depth, max_features, n_estimators):
+    forest_importance = []
+    i = 0
+    columns = ['max_depth', 'max_features', 'n_estimators', 'train_RMSLE', 'test_RMSLE', 'important_features']
+    result = pd.DataFrame(columns=columns)
+    
+    
+    # Divid Train Data
+    X_train, X_test, y_train, y_test = train_test_split(train[features], np.log(train['target']), random_state=0)
+    
+    
+    # Train the Model Using Random Forest
+    print('Train the Model Using Random Forest')
+    
+    for d in range(1, max_depth+1):
+        for f in range(1, max_features+1):
+            for n in range(25, n_estimators+1, 25):
+                print('\nmax_depth = {}, max_features = {}, n_estimators = {}'.format(d, f, n))
+                
+                forest = RandomForestRegressor(max_depth = d, max_features = f, n_estimators = n, random_state=0)
+                forest.fit(X_train, y_train)
+                
+                train_pred = forest.predict(X_train)
+                test_pred = forest.predict(X_test)
+                
+                
+                # Calculate RMSLE
+                train_rmsle = RMSLE(np.exp(y_train), np.exp(train_pred))
+                test_rmsle = RMSLE(np.exp(y_test), np.exp(test_pred))
+                
+                print('RMSLE of Train Set : ', train_rmsle)
+                print('RMSLE of Test Set : {}\n'.format(test_rmsle))
+                
+                
+                # Check for Important Features
+                feat = check_important_features(forest.feature_importances_, train.columns)
+                forest_importance.append(feat)
+                
+                
+                # Save Result
+                r = pd.DataFrame({'max_depth':d, 'max_features':f, 'n_estimators':n, 
+                                  'train_RMSLE':train_rmsle, 'test_RMSLE':test_rmsle, 
+                                  'important_features':', '.join(feat)}, 
+                                 columns=columns, index=[i])
+                result = pd.concat([result, r], ignore_index=True)
+                i += 1
+                
+    return forest_importance, result
+    
+
+def boost_(train, features, max_depth, n_estimators, learning_rate):
+    boost_importance = []
+    i = 0
+    columns = ['max_depth', 'n_estimators', 'learning_rate', 'train_RMSLE', 'test_RMSLE', 'important_features']
+    result = pd.DataFrame(columns=columns)
+    
+    
+    # Divid Train Data
+    X_train, X_test, y_train, y_test = train_test_split(train[features], np.log(train['target']), random_state=0)
+    
+    
+    # Train the Model Using Gradient Boosting
+    print('Train the Model Using Gradient Boosting')
+    
+    for d in range(1, max_depth+1):
+        for n in range(25, n_estimators+1, 25):
+            for l in [learning_rate/10000, learning_rate/1000, learning_rate/100, learning_rate/10, learning_rate]:
+                print('\nmax_depth = {}, n_estimators = {}, learning_rate = {}'.format(d, n, l))
+                
+                boost = GradientBoostingRegressor(max_depth=d, n_estimators=n, learning_rate=l, random_state=0)
+                boost.fit(X_train, y_train)
+                
+                train_pred = boost.predict(X_train)
+                test_pred = boost.predict(X_test)
+                
+                
+                # Calculate RMSLE
+                train_rmsle = RMSLE(np.exp(y_train), np.exp(train_pred))
+                test_rmsle = RMSLE(np.exp(y_test), np.exp(test_pred))
+                
+                print('RMSLE of Train Set : ', train_rmsle)
+                print('RMSLE of Test Set : {}\n'.format(test_rmsle))
+                
+                
+                # Check for Important Features
+                feat = check_important_features(boost.feature_importances_, train.columns)
+                boost_importance.append(feat)
+                
+                
+                # Save Result
+                r = pd.DataFrame({'max_depth':d, 'n_estimators':n, 'learning_rate':l,
+                                  'train_RMSLE':train_rmsle, 'test_RMSLE':test_rmsle,
+                                  'important_features':', '.join(feat)}, 
+                                 columns=columns, index=[i])
+                result = pd.concat([result, r], ignore_index=True)
+                i += 1                
+                
+    return boost_importance, result
